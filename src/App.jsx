220d40195e8344cbb8ea850f48d99a5c075b5327
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-08 12:36:57 -0600

    Initial config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <h1 className="text-center mt-3">Advice App</h1>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
