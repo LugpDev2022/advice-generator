c008170ec487345c268e6ae86ba1b59d3a377bcc
LugpDev2022 <lugpdev2022@gmail.com>
2022-12-08 13:50:37 -0600

    Add paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Container, Modal, Row } from "react-bootstrap";
+import { Col, Container, Modal, Row } from "react-bootstrap";
 
 function App() {
+  const advice =
+    "When you're at a concert or event, enjoy the moment, enjoy being there. Try leaving your camera in your pocket.";
+
+  const id = 117;
+
   return (
     <Modal
       show
@@ -13,7 +18,16 @@ function App() {
       <Modal.Body>
         <Container>
           <Row>
-            <h1 className="title text-info text-center my-3">ADVICE #117</h1>
+            <Col>
+              <h1 className="custom-title text-info text-center my-3">
+                ADVICE #{id}
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="custom-p text-center text-primary">"{advice}"</p>
+            </Col>
           </Row>
         </Container>
       </Modal.Body>
